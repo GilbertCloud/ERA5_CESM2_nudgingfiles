122b44bc98d124541fbae5124dbf31164266c6da
Ash Gilbert <ash.gilbert@colorado.edu>
2024-03-22 11:12:10 -0600

    Prepped scratch process for 1954-57

--- scratch_io.py
+++ scratch_io.py
@@ -3,7 +3,7 @@ import numpy as np
 import xarray as xr
 from tqdm.contrib.concurrent import thread_map
 
-year = ['1952','1953'] 
+year = ['1954','1955','1956','1957'] 
 
 day = [
     '01', '02', '03',
